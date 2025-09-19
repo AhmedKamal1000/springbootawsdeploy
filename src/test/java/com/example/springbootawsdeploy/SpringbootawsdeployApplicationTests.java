d12baa6868876ab004b8ae9cd6b7c4b4232aa6d1
Ahmed Kamal <ahmedkamal@Ahmeds-MacBook-Air.local>
2025-09-19 21:35:26 +0300

    first commit

---
+++ src/test/java/com/example/springbootawsdeploy/SpringbootawsdeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootawsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootawsdeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
